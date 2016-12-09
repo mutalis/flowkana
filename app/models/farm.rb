07a02df16a0fd9a813aee93a69cff455b968a59f
rasg <rodolfo@mutalis.com>
2016-12-08 23:13:25 -0600

    Adds validations and testing to Farm model

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,6 +1,8 @@
+# Farm Model
 class Farm < ApplicationRecord
   has_many :strains
 
   validates :name, presence: true, uniqueness: true
-  validates :county, presence: true, uniqueness: true  
+  validates :county, presence: true, uniqueness: true,
+            inclusion: { in: %w(Mendocino Humboldt Trinity), message: '%{value} is not a valid county' }
 end
--- spec/models/farm_spec.rb
+++ spec/models/farm_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'securerandom'
 
 describe Farm do
   context 'associations' do
@@ -10,5 +11,21 @@ describe Farm do
     it { is_expected.to validate_uniqueness_of :name }
     it { is_expected.to validate_presence_of :county }
     it { is_expected.to validate_uniqueness_of :county }
+  end
+
+  describe 'Farm county values' do
+    it 'is valid with Mendocino, Humboldt, Trinity county' do
+      farm = Farm.new(name: 'any', county: 'Mendocino')
+      expect(farm).to be_valid
+      farm.county = 'Humboldt'
+      expect(farm).to be_valid
+      farm.county = 'Trinity'
+      expect(farm).to be_valid
+    end
+
+    it 'is invalid with any other county' do
+      farm = Farm.new(name: 'any', county: SecureRandom.base64)
+      expect(farm).to_not be_valid
+    end
   end
 end
